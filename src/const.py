6c850da0fc1b5529a0dbb364b5f21e1f223b9877
nikonovone <nikonov.nikita@hotmail.com>
2024-06-13 00:11:59 +0700

    feat: add LABEL2COST

--- src/const.py
+++ src/const.py
@@ -1,6 +1,9 @@
 # Cost of coin to annotation label
 COST2LABEL = {1: 1, 2: 2, 5: 3, 10: 4}
 
+# Label to cost
+LABEL2COST = {0: 0, 1: 1, 2: 2, 3: 5, 4: 10}
+
 # Cost of coin to real size in mm
 COST2SIZE = {1: 20.5, 2: 23, 5: 25, 10: 22}
 
