e7bff5335beafc514577fbf8d5470a991e6ba14e
nikonovone <nikonov.nikita@hotmail.com>
2024-06-12 19:15:47 +0700

    feat: add const maps

---
+++ src/const.py
@@ -0,0 +1,8 @@
+# Cost of coin to annotation label
+COST2LABEL = {1: 1, 2: 2, 5: 3, 10: 4}
+
+# Cost of coin to real size in mm
+COST2SIZE = {1: 20.5, 2: 23, 5: 25, 10: 22}
+
+# Size of 1 ruble
+BASE_SIZE = 20.5
